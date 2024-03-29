1b68ad7549d6a2d6fb4b7fcf059c022194d1e184
Anubhav <lucimgstar007@gmail.com>
2024-03-29 15:38:46 +0530

    Backend part completed -except socketIO

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDb.js";
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+dotenv.config();
+
+app.use(express.json()); //to parse the incoming requests with JSON playloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   //root route http://localhost:5000
+//   res.send("hellooo");
+// });
+
+app.listen(process.env.PORT, () => {
+  connectToMongoDB();
+  console.log(`Server runing  on port ${process.env.PORT}`);
+});
